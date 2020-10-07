d1f0e17b77bd6e1ce47b75e557b53ff88c13d1a6
Keith <visually.basic.keith@gmail.com>
2020-10-07 10:30:01 -0400

    spring project

---
+++ Hackathon-TeamCsGetDegrees-backend/src/main/java/com/hackathon/application/HackathonTeamCsGetDegreesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonTeamCsGetDegreesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonTeamCsGetDegreesBackendApplication.class, args);
+	}
+
+}
---
+++ Hackathon-TeamCsGetDegrees-backend/src/test/java/com/hackathon/application/HackathonTeamCsGetDegreesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonTeamCsGetDegreesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
