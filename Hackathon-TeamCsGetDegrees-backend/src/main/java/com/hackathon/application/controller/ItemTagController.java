4af37ef7eb8e53afa1a4a305acd18aa9dafc0811
Haley Howell <haleyhowell@Haleys-MacBook-Air-2.local>
2020-10-08 09:12:03 -0700

    Adding ItemController

--- Hackathon-TeamCsGetDegrees-backend/src/main/java/com/hackathon/application/controller/ItemTagController.java
+++ Hackathon-TeamCsGetDegrees-backend/src/main/java/com/hackathon/application/controller/ItemTagController.java
@@ -19,6 +19,7 @@ import com.hackathon.application.model.ItemTag;
 import com.hackathon.application.model.Tag;
 import com.hackathon.application.service.ItemService;
 import com.hackathon.application.service.ItemTagService;
+import com.hackathon.application.service.TagService;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
@@ -27,8 +28,27 @@ public class ItemTagController {
 	
 	@Autowired
 	private ItemTagService service;
+	@Autowired
 	private ItemService itemservice;
 	
+	@Autowired
+	private TagService tagservice;
+	
+	@GetMapping("/allTags")
+	public List<Tag> getTags(){
+		List <Tag> tags = tagservice.findAll();
+
+		return tags;
+		
+	}
+	
+	@GetMapping("/items")
+	public List<Item> getItems(){
+		List <Item> items = itemservice.findAll();
+
+		return items;
+		
+	}
 	
 
 	@PostMapping("/tagitems")
@@ -43,22 +63,59 @@ public class ItemTagController {
 			for(ItemTag i: itemTags ) {
 				if(tag.getTagId()== i.getTagId()) {
 					foundIds.add(i.getItemId());
+					System.out.println();
+					System.out.println("Item ID>>>");
+					System.out.println(i.getItemId());
 				}
 			}
 		}
-		
+		System.out.println("");
 		for(int i: foundIds) {
 			for(Item item: items) {
 				if(item.getItemId() == i) {
 					finalItems.add(item);
+					System.out.println();
+					System.out.println("Item>>>");
+					System.out.println(item.toString());
 				}
 			}
 		}
 		
+		//Adding special Keys 
+		List<String> specialKeys = new ArrayList();
+		specialKeys.add("DicksExclusive");
+		specialKeys.add("ExtendedSized");
+		specialKeys.add("TopSeller");
+		specialKeys.add("NewArrivals");
 		
+		List<Item> reducedItems = new ArrayList();
+		int count = 0;
+		for(Item i: finalItems) {
+			if(count <= 3) {
+				if(specialKeys.contains(i.getSpecialKey())) {
+					reducedItems.add(i);
+				}
+			}
+		
+		count++;
+		}
+		
+		if(reducedItems.size() == 3) {
+			return reducedItems;
+		}
+		else {
+			
+			if(finalItems.size()>= 4) {
+				return finalItems.subList(0, 3);
+			}
+			
+		}
+		
+		return new ArrayList<Item>();
 	
 
-		return finalItems;
+
+		
 		
 	}
 
